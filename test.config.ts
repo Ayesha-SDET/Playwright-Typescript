1864865daf13eef05642142b397df32e227b45f1
Ayesha Tahir <ayesha.uni.hudd@gmail.com>
2026-01-14 12:52:43 +0000

    Add files via upload

---
+++ test.config.ts
@@ -0,0 +1,15 @@
+export class TestConfig{
+ 
+    appUrl="https://tutorialsninja.com/demo/"
+    
+  
+    //valid login credentials
+    email="ayeshahep@gmail.com"
+    password="hep786990"
+
+    //product details
+    productName="Tablets"
+    productQuantity="5"
+    totalPrice="$241.99"
+
+}
